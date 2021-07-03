0680ad53c94cf8bd941cd00d491e86ea787744c2
Askar Rizvi <askarrizvi@askars-mbp.lan>
2021-07-03 12:56:57 -0400

    Get temperature values from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@
 const apiKey = "9829a4855c0d59b42e6f434613c07c77";
 var lat;
 var lon;
+var currCon;
+var currTemp;
+var currWind;
+var currHum;
+var currUv;
+var tempArr = [];
 var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=ottawa&units=metric&appid=9829a4855c0d59b42e6f434613c07c77";
 var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
 var apiOneCall = "https://api.openweathermap.org/data/2.5/onecall?units=metric";
@@ -12,6 +18,10 @@ function getWeather() {
             response.json().then(function (data) {
                 lat = data.coord.lat;
                 lon = data.coord.lon;
+                currCon = data.weather[0].main;
+                currTemp = data.main.temp;
+                currWind = data.wind.speed;
+                currHum = data.main.humidity;
                 console.log(data);
                 console.log(lon);
                 console.log(lat);
@@ -19,6 +29,8 @@ function getWeather() {
                     if (oneCallResponse.ok) {
                         oneCallResponse.json().then(function (oneCallData) {
                             console.log(oneCallData);
+                            currUv = oneCallData.uvi;
+                            tempArr = oneCallData.daily;
                         });
                     }
                     else {
@@ -31,7 +43,10 @@ function getWeather() {
             alert("enter a valid city");
         }
     });
+displayWeather();
+}
 
+function displayWeather(){
 
 }
 
